eb37fbe10f63e4e130f65fa5f33d82e921d8cba8
Nolan Meyer <meyer09@gmail.com>
2023-09-04 17:00:48 -0500

    update naming from dotfiles to .dotfiles

--- install.sh
+++ install.sh
@@ -13,14 +13,14 @@ fi
 
 # Clone dotfiles repository (assuming it's hosted on GitHub)
 echo "${GREEN}Cloning dotfiles...${NC}"
-git clone git@github.com:nolasaurus/dotfiles.git ~/dotfiles
+git clone git@github.com:nolasaurus/.dotfiles.git ~/.dotfiles
 
 # Create symlinks in the home directory pointing to files in ~/dotfiles/
 echo "${GREEN}Creating symlinks in the home directory...${NC}"
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
-ln -s ~/dotfiles/.bash_functions ~/.bash_functions
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/.dotfiles/.bash_functions ~/.bash_functions
+ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 # ... Add other dotfiles you want to symlink ...
 
 # Source the bashrc to apply changes
